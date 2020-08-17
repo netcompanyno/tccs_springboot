e82f2b2785c28b599cf097aec1e90fc5edf8cb0e
Simen Wold Anderson <simenwold.anderson@drift.oslo.kommune.no>
2020-08-17 09:44:23 +0200

    Added files for task 2

--- src/main/java/no/netcompany/tccs/sb/CustomerController/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package no.netcompany.tccs.sb.CustomerController;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
--- src/main/java/no/netcompany/tccs/sb/SbApp.java
+++ src/main/java/no/netcompany/tccs/sb/SbApp.java
@@ -2,11 +2,20 @@ package no.netcompany.tccs.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SbApp{
 
     public static void main(String[] args) {
         SpringApplication.run(SbApp.class, args);
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
