ddf4a001118dc28cb4f2c2b78c97ca272beccc1f
geirn <geir.ottersen@gmail.com>
2020-08-17 10:31:53 +0200

    Fjernet for å lage initiell start

--- src/main/java/no/netcompany/tccs/sb/SbApp.java
+++ src/main/java/no/netcompany/tccs/sb/SbApp.java
@@ -7,15 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SbApp{
 
     public static void main(String[] args) {
         SpringApplication.run(SbApp.class, args);
     }
 
-    @RequestMapping("/hello/{name}")
-    public String hello(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
 }
