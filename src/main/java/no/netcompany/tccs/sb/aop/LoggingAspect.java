091960e1afcf419d9b75add3c294bac0ce285457
geirn <geir.ottersen@gmail.com>
2020-09-02 14:48:49 +0200

    Logger litt mer

--- src/main/java/no/netcompany/tccs/sb/aop/LoggingAspect.java
+++ src/main/java/no/netcompany/tccs/sb/aop/LoggingAspect.java
@@ -3,6 +3,7 @@ package no.netcompany.tccs.sb.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
@@ -14,13 +15,15 @@ public class LoggingAspect {
     public Object logExecutionTime(ProceedingJoinPoint joinPoint)
             throws Throwable {
         long start = System.currentTimeMillis();
+        final String className = joinPoint.getTarget().getClass().getName();
+        final String methodName = MethodSignature.class.cast(joinPoint.getSignature()).getMethod().getName();
+        LoggerFactory.getLogger(className).info("Method {} invoked", methodName);
 
         Object proceed = joinPoint.proceed();
 
         long timeMs = System.currentTimeMillis() - start;
 
-        LoggerFactory.getLogger(joinPoint.getTarget().getClass().getName()).info(
-                "Method took {} millis", timeMs);
+        LoggerFactory.getLogger(className).info("Method {} took {} millis", methodName, timeMs);
 
         return proceed;
     }
